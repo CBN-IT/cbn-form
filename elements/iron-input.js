692f7207627ea4b62b5fe630405604a59f3cf73e
Stancu Florin <niflostancu@gmail.com>
2015-07-30 15:18:20 +0300

    Split the Polymer element extensions into multiple files.

---
+++ elements/iron-input.js
@@ -0,0 +1,62 @@
+(function(CbnForm) {
+	
+	if (!Polymer.reflect) return;
+	
+	/**
+	 * The behavior to use for extending Polymer's `iron-input` element with `cbn-form` features.
+	 */
+	CbnForm.PolymerElements.IronInputExtension = {
+		
+		properties: {
+			
+			/**
+			 * Use this property instead of `value` for two-way data binding.
+			 */
+			bindValue: {
+				observer: '_fe_bindValueChanged',
+				type: String
+			},
+			
+			/**
+			 * Do not break non-cbn-form usage.
+			 */
+			_exposeInputEvents: {
+				type: Boolean,
+				value: true,
+				readOnly: true
+			}
+			
+		},
+		
+		ready: function() {
+			
+		},
+		
+		/**
+		 * Observes when the bindValue changes and sets the value.
+		 */
+		_fe_bindValueChanged: function(newValue) {
+			this._setValue(newValue);
+		},
+		
+		/**
+		 * Overridden to also change the {@link #bindValue} property. 
+		 * 
+		 * @param value The value to set.
+		 * @protected
+		 */
+		_setValue: function (value) {
+			if (this._fe_previousValue !== value) {
+				this.value = value;
+				this.bindValue = value;
+				this._fe_previousValue = value;
+				this._fe_valueChanged(value);
+			}
+		}
+		
+	};
+	
+	Polymer.injectBehaviors('iron-input', [ CbnForm.NativeFormElement, 
+			CbnForm.PolymerElements.IronInputExtension ]);
+	
+})(CbnForm);
